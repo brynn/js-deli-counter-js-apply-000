30acf7cb04da43dde3d5cc04d91ebde7fe2ae6cf
brynn.shepherd@gmail.com <brynn.shepherd@gmail.com>
2018-08-21 17:44:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(katzDeliLine, newPerson) {
   var number = katzDeliLine.length + 1;
-  katDeliLine.push(newPerson);
+  katzDeliLine.push(newPerson);
   return `Welcome, ${newPerson}. You are number ${number} in line.`;
 }
 
