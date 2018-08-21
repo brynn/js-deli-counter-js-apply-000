51a813d738a7f06447f13e692605c0151d12e14e
brynn.shepherd@gmail.com <brynn.shepherd@gmail.com>
2018-08-21 17:40:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-function takeANumber() {
-  
+function takeANumber(katzDeliLine, newPerson) {
+  var number = katzDeliLine.length + 1;
+  return `Welcome, ${newPerson}. You are number ${} in line`;
 }
